b910f5a4ad4e4cb05395da7c46c8f5e92fe17848
Andre Sortland <arsortland@gmail.com>
2022-07-17 20:03:14 +0200

    more logic, making a working model before refactoring into modules later

--- src/dom.js
+++ src/dom.js
@@ -1,8 +1,14 @@
+import { todos } from "./logic";
+
 export const createSidebar = () => {
   const makenew = document.querySelector(".makenew");
   const inputnew = document.createElement("input");
-  const submitnew = document.createElement("submit");
+  const submitnew = document.createElement("button");
 
   inputnew.setAttribute("type", "text");
+  submitnew.textContent = "Add Project";
+  inputnew.classList.add("inputnew");
+  submitnew.classList.add("submitnew");
   makenew.appendChild(inputnew);
+  makenew.appendChild(submitnew);
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 import "./style.css";
-import { createSidebar } from "./dom";
+
+const createSidebar = () => {
+  const makenew = document.querySelector(".makenew");
+  const inputnew = document.createElement("input");
+  const submitnew = document.createElement("button");
+
+  inputnew.setAttribute("type", "text");
+  inputnew.setAttribute("required", "");
+  submitnew.textContent = "Add Project";
+  inputnew.classList.add("inputnew");
+  submitnew.classList.add("submitnew");
+  makenew.appendChild(inputnew);
+  makenew.appendChild(submitnew);
+};
+
+const createProject = () => {
+  const projects = [];
+  const newproject = document.querySelector(".inputnew");
+  const submit = document.querySelector(".submitnew");
+  submit.addEventListener("click", () => {
+    console.log("HEI");
+    //Sett opp slik at det ikke går hvis dette fieldet er tomt
+    console.log(newproject.value);
+    projects.push(newproject.value);
+    console.log(projects);
+  });
+};
 
 createSidebar();
+createProject();
