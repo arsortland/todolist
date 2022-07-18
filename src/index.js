7720b832275a03496e81f1b525db924b1268a8e4
Risinggaard Sortland <andre.risinggaardsortland@nov.com>
2022-07-18 10:29:31 +0200

    fiddling with some logic

--- src/index.js
+++ src/index.js
@@ -22,17 +22,48 @@ const createProject = (() => {
   submit.addEventListener("click", () => {
     //Sett opp slik at det ikke går hvis dette fieldet er tomt
     projects.push(newproject.value);
-    console.log(projects);
+    // console.log(projects);
+    listProjects();
   });
 })();
 
+const defaultProject = (() => {
+  const listexisting = document.querySelector(".makenew");
+  let general = document.createElement("div");
+  general.textContent = "General";
+  listexisting.appendChild(general);
+})();
+
 const listProjects = () => {
-  //   for (let val in projects) {
-  //     console.log("Her:" + val);
-  //   }
-  for (let i = 0; i < projects.length; i++) {
-    console.log(projects[i]);
+  const listexisting = document.querySelector(".listexisting");
+  listexisting.textContent = "";
+  for (let val of projects) {
+    //Create a div template here with more elements for the todo? Like a form maybe?
+    let newdiv = document.createElement("button");
+    newdiv.classList.add("aproject");
+    newdiv.textContent = val;
+    listexisting.appendChild(newdiv);
   }
+  showProject();
+};
+
+const showProject = () => {
+  const allProjects = document.querySelectorAll(".aproject");
+  const mainarea = document.querySelector(".mainarea");
+  allProjects.forEach((project) => {
+    project.addEventListener("click", () => {
+      console.log(project.innerHTML);
+      const currproject = document.createElement("div");
+      currproject.textContent = project.innerHTML;
+      mainarea.appendChild(currproject);
+    });
+  });
 };
 
 listProjects();
+
+/**
+ * Make each project an array
+ * each array contains the todos
+ * list all todos in mainarea of selected project
+ */
