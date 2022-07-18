462f67687f75cdaa02caf9baa88f08017eb860b6
Risinggaard Sortland <andre.risinggaardsortland@nov.com>
2022-07-18 15:47:41 +0200

    new idea

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,7 @@
+const projects = [];
+
 export const createSidebar = (() => {
-  const makenew = document.querySelector(".makenew");
+  const form = document.querySelector(".form");
   const inputnew = document.createElement("input");
   const submitnew = document.createElement("button");
 
@@ -7,6 +9,50 @@ export const createSidebar = (() => {
   submitnew.textContent = "Add Project";
   inputnew.classList.add("inputnew");
   submitnew.classList.add("submitnew");
-  makenew.appendChild(inputnew);
-  makenew.appendChild(submitnew);
+  form.appendChild(inputnew);
+  form.appendChild(submitnew);
 })();
+
+export const createProject = (() => {
+  const newproject = document.querySelector(".inputnew");
+  const submit = document.querySelector(".submitnew");
+  submit.addEventListener("click", () => {
+    //Sett opp slik at det ikke går hvis dette fieldet er tomt
+    projects.push(newproject.value);
+    listProjects();
+  });
+})();
+
+// const defaultProject = (() => {
+//   const listexisting = document.querySelector(".form");
+//   let general = document.createElement("div");
+//   general.textContent = "General";
+//   listexisting.appendChild(general);
+// })();
+
+export const listProjects = () => {
+  const listexisting = document.querySelector(".listexisting");
+  listexisting.textContent = "";
+  for (let val of projects) {
+    //Create a div template here with more elements for the todo? Like a form maybe?
+    let newdiv = document.createElement("button");
+    newdiv.classList.add("aproject");
+    newdiv.textContent = val;
+    listexisting.appendChild(newdiv);
+  }
+  showProject();
+};
+
+export const showProject = () => {
+  const allProjects = document.querySelectorAll(".aproject");
+  const mainarea = document.querySelector(".mainarea");
+  allProjects.forEach((project) => {
+    project.addEventListener("click", () => {
+      mainarea.innerHTML = "";
+      console.log(project.innerHTML);
+      const currproject = document.createElement("div");
+      currproject.textContent = project.innerHTML;
+      mainarea.appendChild(currproject);
+    });
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,52 +1,6 @@
 import "./style.css";
 import { createSidebar } from "./dom";
 
-const projects = [];
-
-const createProject = (() => {
-  const newproject = document.querySelector(".inputnew");
-  const submit = document.querySelector(".submitnew");
-  submit.addEventListener("click", () => {
-    //Sett opp slik at det ikke går hvis dette fieldet er tomt
-    projects.push(newproject.value);
-    listProjects();
-  });
-})();
-
-const defaultProject = (() => {
-  const listexisting = document.querySelector(".makenew");
-  let general = document.createElement("div");
-  general.textContent = "General";
-  listexisting.appendChild(general);
-})();
-
-const listProjects = () => {
-  const listexisting = document.querySelector(".listexisting");
-  listexisting.textContent = "";
-  for (let val of projects) {
-    //Create a div template here with more elements for the todo? Like a form maybe?
-    let newdiv = document.createElement("button");
-    newdiv.classList.add("aproject");
-    newdiv.textContent = val;
-    listexisting.appendChild(newdiv);
-  }
-  showProject();
-};
-
-const showProject = () => {
-  const allProjects = document.querySelectorAll(".aproject");
-  const mainarea = document.querySelector(".mainarea");
-  allProjects.forEach((project) => {
-    project.addEventListener("click", () => {
-      mainarea.innerHTML = "";
-      console.log(project.innerHTML);
-      const currproject = document.createElement("div");
-      currproject.textContent = project.innerHTML;
-      mainarea.appendChild(currproject);
-    });
-  });
-};
-
 /**
  * Make each project an array
  * each array contains the todos
