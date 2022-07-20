11dfa02cef0360fbc37412261f6962b90b4a6f86
Risinggaard Sortland <andre.risinggaardsortland@nov.com>
2022-07-20 11:35:40 +0200

    cleaned up some functions to prevent empty fields

--- src/index.js
+++ src/index.js
@@ -2,45 +2,53 @@ import "./style.css";
 
 const projects = [["General"]];
 
-const addToDo = (name) => {
-  //Need to be implemented at "X" in each project [][X]
-  return { name };
-};
-
-const projectEvent = (() => {
-  const inputfield = document.querySelector(".newtodo");
-  const submit = document.querySelector(".submittodo");
-  submit.addEventListener("click", () => {
-    // console.log(inputfield.value);
-    addProject(inputfield.value);
-    listView(); // makes the list again
-  });
-})();
-
 const addProject = (name) => {
   //eventlistener and use this function to add the content of input (outside this function)
   projects.push([name]);
+};
+//DOM
+const listView = () => {
+  const listexisting = document.querySelector(".listexisting");
+  listexisting.textContent = ""; //Clears entire area each time.
+  const newform = document.createElement("form");
+  newform.setAttribute("onsubmit", "return false;");
+  listexisting.appendChild(newform);
+
+  for (let i = 0; i < projects.length; i++) {
+    const newdiv = document.createElement("div");
+    const button = document.createElement("button");
+    button.classList.add("projectname");
+    button.textContent = projects[i][0];
+    newform.append(newdiv, button);
+  }
+};
+//DOM
+const projectEvent = (() => {
+  const inputfield = document.querySelector(".newproject");
+  const submit = document.querySelector(".submitproject");
+  submit.addEventListener("click", () => {
+    if (inputfield.value != "") {
+      addProject(inputfield.value); //adds the inputfield to projectarray
+      listView(); // makes the list again of projects in array
+      inputfield.value = "";
+    } else {
+      return;
+    }
+  });
+})();
+
+const addToDo = (name) => {
+  //Need to be implemented at "X" in each project [][X]
+  return { name };
 };
 
 const listProject = () => {
   //function here
 };
 
-const listView = () => {
-  const listexisting = document.querySelector(".listexisting");
-  listexisting.textContent = ""; //Clears entire area each time.
-  const newform = document.createElement("form");
-  listexisting.appendChild(newform);
-
-  for (let i = 0; i < projects.length; i++) {
-    const newdiv = document.createElement("div");
-    const button = document.createElement("button");
-    button.textContent = projects[i][0];
-    newform.append(newdiv, button);
-  }
-};
-
-listView();
+const onPageLoad = (() => {
+  listView();
+})();
 
 //Make it so that the projects first index (the name) is shown as the header
 //in the mainarea, then the rest of the project are listed as objects beneath.
