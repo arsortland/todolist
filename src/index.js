ca96f10ec41bd2b60e4caac76f12bf9938831d6d
André Sortland <arsortland@gmail.com>
2022-07-22 15:53:30 +0200

    Started to implement logic for separating projects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const projects = [["General"]];
+const projects = [["General", "r443"]];
 
 const addProject = (name) => {
   //eventlistener and use this function to add the content of input (outside this function)
@@ -23,13 +23,14 @@ const listView = () => {
   }
 };
 //DOM
-const createProject = (() => {
+const ProjectMain = (() => {
   const inputfield = document.querySelector(".newproject");
   const submit = document.querySelector(".submitproject");
   submit.addEventListener("click", () => {
     if (inputfield.value != "") {
       addProject(inputfield.value); //adds the inputfield to projectarray
       listView(); // makes the list again of projects in array
+      showProjectInMain();
       inputfield.value = "";
     } else {
       return;
@@ -41,18 +42,34 @@ const addToDo = (name) => {
   //Need to be implemented at "X" in each project [][X]
   return { name };
 };
-const addToDoDOM = () => {};
 
-const listProjectToDo = (() => {
-  //function here
-  const mainarea = document.querySelector(".mainarea");
-  // mainarea.textContent = projects[0][0];
-})();
-
-const onPageLoad = (() => {
+window.onload = () => {
   listView();
-})();
+  showProjectInMain();
+};
 
+const showProjectInMain = () => {
+  const allprojects = document.querySelectorAll(".projectname");
+  allprojects.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      //findIndex
+      console.log(btn.textContent);
+      for (let i = 0; i < projects.length; i++) {
+        if (projects[i][0] == btn.textContent) {
+          console.log(i);
+          const mainarea = document.querySelector(".mainarea");
+          mainarea.textContent = projects[i][0];
+          const addtodobtn = document.createElement("button");
+          addtodobtn.textContent = "+";
+          addtodobtn.classList.add("addtodobtn");
+          addtodobtn.setAttribute("value", i);
+          mainarea.appendChild(addtodobtn);
+          //Have to so the value is used to append the actual to do data to the right projectlist
+        }
+      }
+    });
+  });
+};
 //Make it so that the projects first index (the name) is shown as the header
 //in the mainarea, then the rest of the project are listed as objects beneath.
 //Take date and prio last, use same logic as listview function.
